9301bbfc24a46ad7217074b590c68f4402339f9f
JumanjiFlavor <manguiatrexson@gmail.com>
2025-04-11 08:41:28 +0800

    cash_advance
    shfasjkdajshd

---
+++ login.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SmartPark Systems Solutions Cash Advance</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css">
+    <link rel="stylesheet" href="src/styles/style.css">
+  </head>
+<body>
+    <div class="section columns custom-background-color p-1 border-radius-customize">
+        <div class="p-6 column is-half has-background-white border-radius-customize-inner">
+            <div class="contents">
+                <div class="field is-flex is-justify-content-center is-align-item-center">
+                    <div class="control">
+                    <img src="src/image/SmartPark.png">  
+                    </div>
+                </div>
+            </div>
+            <div class="contents">
+                <div class="field is-flex is-justify-content-center is-align-item-center my-3">
+                    <div class="control">
+                    <h3 class="is-size-3 has-text-weight-bold custom-text-color"><center>LOG IN</center></h3>  
+                    </div>
+                </div>
+            </div>
+                <div class="field">
+                  <div class="control">
+                        <form action="" method="post">
+                            <div class="field">
+                              <label for="username">Username</label>
+                              <div class="control">
+                                <input type="email" class="input mb-3 is-link" name="username" placeholder="Enter email">
+                              </div>
+                            </div>
+                            <div class="field">
+                              <label for="password">Password</label>
+                              <div class="control">
+                                <input type="password" class="input is-link" name="password" placeholder="Enter password">
+                              </div>
+                              <button class="button is-ghost">Forgot Password?</button>
+                            </div>
+                            <div class="field">
+                              <div class="control is-flex is-justify-content-center">
+                                <input type="submit" class="button custom-background-color has-text-white is-size-6 mb-3" value="Log in">
+                              </div>
+                            </div>
+                        </form> 
+                  </div>
+                </div>
+          </div>   
+          <div class="column is-half is-hidden-mobile is-flex is-justify-content-center is-align-items-center">
+              <h3 class="has-text-white is-size-3 is-centered has-text-weight-bold"><center>Cash Advance Request System</center></h3>
+            </div>   
+          
+              
+    </div>
+</body>
+</html>
