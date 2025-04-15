6faa570976a2de47251c0592c0a810e355d3107f
Rexson <manguiatrexson@gmail.com>
2025-04-16 07:56:53 +0800

    first commit

--- login.php
+++ login.php
@@ -6,6 +6,7 @@
     <title>SmartPark Systems Solutions Cash Advance</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css">
     <link rel="stylesheet" href="src/styles/style.css">
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
   </head>
 <body>
     <div class="section columns custom-background-color p-1 border-radius-customize">
@@ -28,9 +29,9 @@
                   <div class="control">
                         <form action="" method="post">
                             <div class="field">
-                              <label for="username">Username</label>
+                              <label for="email">Email</label>
                               <div class="control">
-                                <input type="email" class="input mb-3 is-link" name="username" placeholder="Enter email">
+                                <input type="email" class="input mb-3 is-link" name="email" placeholder="Enter email">
                               </div>
                             </div>
                             <div class="field">
@@ -46,7 +47,7 @@
                             </div>
                             <div class="field">
                               <div class="control is-flex is-justify-content-center">
-                                <input type="submit" class="button custom-background-color has-text-white is-size-6 mb-3" value="Log in">
+                                <input type="submit" name="login" class="button custom-background-color has-text-white is-size-6 mb-3" value="Log in">
                               </div>
                             </div>
                         </form> 
@@ -55,9 +56,47 @@
           </div>   
           <div class="column is-half is-hidden-mobile is-flex is-justify-content-center is-align-items-center">
               <h3 class="has-text-white is-size-3 is-centered has-text-weight-bold"><center>Cash Advance Request System</center></h3>
-            </div>   
-          
-              
+            </div>            
     </div>
 </body>
 </html>
+
+<?php 
+$servername = "127.0.0.1, 3304";
+$conn = new PDO("sqlsrv:server=$servername; Database=C_A", 'root','jeff');
+
+if(isset($_POST['login'])){
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+
+  $stmtemail = $conn->prepare("SELECT * FROM USERS WHERE user_email=:acc_email");
+  $stmtemail->bindValue(":acc_email", $email);
+  $stmtemail->execute();
+  $fetched_email = $stmtemail->fetchColumn();
+
+  if(isset($_POST['email'])==$fetched_email){
+    $stmtpassword = $conn->prepare("SELECT * FROM USERS WHERE user_password=:acc_password");
+    $stmtpassword->bindValue(":acc_password", $password);
+    $stmtpassword->execute();
+    $fetched_password = $stmtpassword->fetchColumn();
+      if(isset($_POST['password'])==$fetched_password){
+        $_SESSION['email'] = $email;
+        $_SESSION['password'] = $password;
+        header("Location:requestor.php");
+        exit;
+      }else{
+        echo "<script>
+                 swal('Error Login','Invalid Email', 'error');
+              </script>";
+      }
+  }else{
+    echo "<script>
+              swal('Error Login','Invalid Email', 'error');
+          </script>";
+  }
+}
+
+if(isset($_SESSION['email'])){
+  unset($_SESSION['email']); unset($_SESSION['password']);
+}
+?>
---
+++ requestor.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css">
+    <title>SmartPark Systems Solutions Cash Advance</title>
+</head>
+<body>
+    <h1 class="is-text is-size-1">Hello World</h1>
+</body>
+</html>
